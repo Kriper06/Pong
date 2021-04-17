7d7e9514870fbb5a53277f07ef530b26909244f4
Kriper_06 <81853884+Kriper06@users.noreply.github.com>
2021-04-17 12:26:31 +0300

    Add files via upload

--- Pong.py
+++ Pong.py
@@ -25,17 +25,37 @@ class Player(GameSprite):
             self.rect.y -= self.speed
         if keys[K_DOWN] and self.rect.y < 405:
             self.rect.y += self.speed
-class Enemy(GameSprite):
-    def update(self):
-        global lose
-        self.rect.y += self.speed
-        if self.rect.y > 500:
-            self.rect.y = 0
-            self.rect.x = randint(100, 600)
+Player_l=Player("Ракетка_Pong.png",20,200,30,140,5)
+Player_r=Player("Ракетка_Pong.png",670,200,30,140,5)
+ball=GameSprite("Мяч_Pong.png",250,360,30,30,0)
 game=True
+speed_x=3
+speed_y=3
+font1=font.Font(None,35)
+lose1=font1.render("PLAYER L LOSE!", True, (180, 0, 0))
+font2=font.Font(None,35)
+lose2=font2.render("PLAYER R LOSE!", True, (180, 0, 0))
 while game:
+    window.fill((0,0,0))
+    Player_l.reset()
+    Player_r.reset()
+    ball.reset()
+    Player_l.update_l()
+    Player_r.update_r()
+    ball.rect.x += speed_x
+    ball.rect.y += speed_y
+    if ball.rect.y>470 or ball.rect.y<0:
+        speed_y *= -1
+    if sprite.collide_rect(Player_l,ball) or sprite.collide_rect(Player_r,ball):
+        speed_x *= -1
+    if ball.rect.x < 0:
+        finish=True
+        window.blit(lose1, (200,200))
+    if ball.rect.x > 700:
+        finish=True
+        window.blit(lose2, (200,200))
     for e in event.get():
         if e.type==QUIT:
             game=False
-        clock.tick(60)
-        display.update()
+    clock.tick(60)
+    display.update()
